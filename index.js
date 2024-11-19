52f2db0a2ff678ec8d4afbeae6109a0017d270ed
Tyler Hogue <hogue73@embarqmail.com>
2024-11-19 16:33:44 -0500

    passed tests

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names, event){
+
+    let cards = []
+    for (let i = 0; i < names.length; i++){
+    let message = `Thank you, ${names[i]}, for the wonderful ${event} gift!`
+    cards.push(message)
+    }
+    return cards
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise")
+
+function countDown(){
+let currentNumber = 10
+while (currentNumber > -1)
+{
+    console.log(currentNumber)
+    currentNumber--
+}
+}
+
